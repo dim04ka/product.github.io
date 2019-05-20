411a6ecee2c81bbc557b1abd8032cc0cd5db5c69
Dim04ka <suhotsky.dmitry@gmail.com>
2019-05-20 19:16:59 +0300

    add paralax and nunmer

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 window.onload = function() {
 
+
+
+  // auto number show
+	function countup(className){
+		var countBlockTop = $("."+className).offset().top;
+		var windowHeight = window.innerHeight;
+		var show = true;
+					
+		$(window).scroll( function (){
+			if(show && (countBlockTop < $(window).scrollTop() + windowHeight)){ 
+				show = false;
+						
+				$('.'+className).spincrement({
+					from: 1,
+					duration: 4000,
+				});
+			}
+		})	
+	}
+
+  $(function() {
+		countup("item__title-one", $(".item__title-one").text());
+    countup("item__title-one-two", $(".item__title-one-two").text());
+    countup("item__title-three", $(".item__title-three").text());
+    countup("item__title-four", $(".item__title-four").text());
+  });
+
+
+
+
+  //paralax
+  function paralax(e) {
+    var layer = document.querySelector(".layer");
+    layer.style.transform = `translateX(${e.clientX*10/1000}px)`
+  }
+  document.addEventListener('mousemove', paralax);
+
+
+
+
   //open new input
   var select = document.querySelector("#select");
   select.addEventListener('change', function(){
@@ -36,6 +76,6 @@ window.onload = function() {
         document.querySelector(".header__nav").style.cssText = "padding: 10px 0; transition: all 1s";
         document.querySelector(".header").style.cssText =" box-shadow: none;"
       }
-    } 
+  }  
 };
 
